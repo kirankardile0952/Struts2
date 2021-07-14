c1dea7f5d6de7ec2bb73b7e19420bd000cfe657d
Kiran Kardile <kardilekiran0952@gmail.com>
2021-07-14 12:50:50 +0530

    first commit

---
+++ RegistrationApplication/src/main/java/RegisterAction.java
@@ -0,0 +1,72 @@
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private Integer age;
+	private String email;
+
+	public String execute() {
+		System.out.println("execute() Method Called");
+		return "success";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/*
+	 * public void validate() { if (firstName.equals("")) {
+	 * addFieldError("firstName", "First Name is required."); }
+	 * 
+	 * if (lastName.equals("")) { addFieldError("lastName",
+	 * "Last Name is required."); }
+	 * 
+	 * if (gender == null) { addFieldError("gender", "Gender is required."); }
+	 * 
+	 * if (age == null) { addFieldError("age", "Age is required."); } else if(age <=
+	 * 18) { addFieldError("age", "Age should be above 18"); }
+	 * 
+	 * if (email.equals("")) { addFieldError("email", "Email is required."); }
+	 * 
+	 * }
+	 */
+}
